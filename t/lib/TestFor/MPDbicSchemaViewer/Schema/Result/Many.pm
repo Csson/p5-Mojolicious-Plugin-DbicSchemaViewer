47f41831314d45767d14edaa60d3b1ee225a01f4
Erik Carlsson <info@code301.com>
2016-09-04 10:19:55 +0000

    Fix whitespaces

--- t/lib/TestFor/MPDbicSchemaViewer/Schema/Result/Many.pm
+++ t/lib/TestFor/MPDbicSchemaViewer/Schema/Result/Many.pm
@@ -4,8 +4,8 @@ use base 'DBIx::Class::Core';
 
 __PACKAGE__->table('Many');
 __PACKAGE__->add_columns(
-	many_id => { data_type => 'int' },
-	thedata => { data_type => 'varchar' },
+    many_id => { data_type => 'int' },
+    thedata => { data_type => 'varchar' },
 );
 
 __PACKAGE__->set_primary_key(qw/many_id/);
--- t/lib/TestFor/MPDbicSchemaViewer/Schema/Result/One.pm
+++ t/lib/TestFor/MPDbicSchemaViewer/Schema/Result/One.pm
@@ -4,8 +4,8 @@ use base 'DBIx::Class::Core';
 
 __PACKAGE__->table('One');
 __PACKAGE__->add_columns(
-	one_id => { data_type => 'int' },
-	thedata => { data_type => 'varchar' },
+    one_id => { data_type => 'int' },
+    thedata => { data_type => 'varchar' },
 );
 
 __PACKAGE__->set_primary_key(qw/one_id/);
