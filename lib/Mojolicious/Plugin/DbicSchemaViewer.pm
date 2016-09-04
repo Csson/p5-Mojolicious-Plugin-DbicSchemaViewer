b8f5491d8a8bccf451ce826986945de6ddff3186
Erik Carlsson <info@code301.com>
2016-09-04 20:50:59 +0000

    Bump version after 0.0102

--- lib/Mojolicious/Plugin/DbicSchemaViewer.pm
+++ lib/Mojolicious/Plugin/DbicSchemaViewer.pm
@@ -5,7 +5,7 @@ package Mojolicious::Plugin::DbicSchemaViewer;
 
 # ABSTRACT: Viewer for DBIx::Class schema definitions
 # AUTHORITY
-our $VERSION = '0.0102';
+our $VERSION = '0.0103';
 
 use Mojo::Base 'Mojolicious::Plugin';
 use File::ShareDir::Tarball 'dist_dir';
