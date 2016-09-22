c52bcbf0b4e2ab9b0910a543cd3aa9c6ab8dd4ca
Erik Carlsson <info@code301.com>
2016-09-22 07:30:40 +0000

    Remove unused files

--- .localscript/daemon_testschema.pl
+++
@@ -1,9 +0,0 @@
-use Mojolicious::Lite;
-
-use Dir::Self;
-use lib __DIR__ . '/../t/lib';
-use TestFor::MPDbicSchemaViewer::Schema;
-
-plugin 'DbicSchemaViewer', schema => TestFor::MPDbicSchemaViewer::Schema->connect;
-
-app->start;
